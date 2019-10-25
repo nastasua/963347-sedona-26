b541b4a32fbd60308f1f8679a56412eb93544106
nastasua <nastasyua.r@yandex.ru>
2019-10-25 19:15:05 +0300

    git push
    Доработала проект, привела его в соответствие макету и тз.

--- js/popup.js
+++ js/popup.js
@@ -7,36 +7,33 @@ var btnMinChildren = document.querySelector(".btn-min-children");
 var btnMaxChildren = document.querySelector(".btn-max-children");
 var inputAdult = document.querySelector(".form-adult");
 var inputChildren = document.querySelector(".form-children");
+var numberAdult = + inputAdult.value;
+var numberChildren = + inputChildren.value;
 
-btn.addEventListener("click", function(evt) {
-  if ( popup.classList.contains("modal-hide") ) {
-    evt.preventDefault();
-    popup.classList.remove("modal-hide");
+btn.addEventListener("click", function() {
+  if ((" " + popup.className + " ").indexOf("modal-hide") > -1) {
+    popup.className = "modal-form";
   } else {
-    popup.classList.add("modal-hide");
+    popup.className = "modal-hide";
   }
 });
 
 btnMinAdult.addEventListener("click", function() {
-  if ( inputAdult.value > 0 ) {
-    inputAdult.value --;
+  if ( numberAdult > 0 ) {
+    inputAdult.value = --numberAdult;
   }   
 });
 
 btnMaxAdult.addEventListener("click", function() {
-  if ( inputAdult.value >= 0 ) {
-    inputAdult.value ++;
-  }   
+  inputAdult.value = ++numberAdult;   
 });
 
 btnMinChildren.addEventListener("click", function() {
-  if ( inputChildren.value > 0 ) {
-    inputChildren.value --;
+  if ( numberChildren > 0 ) {
+    inputChildren.value = --numberChildren;
   }   
 });
 
 btnMaxChildren.addEventListener("click", function() {
-  if ( inputChildren.value >= 0 ) {
-    inputChildren.value ++;
-  }   
+  inputChildren.value = ++numberChildren;  
 });
